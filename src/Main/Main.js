14e3812de24ad52a0065ea27a196f74ab210ad71
nickolayArbuzov <nickarbuzov@yandex.by>
2019-10-20 23:25:22 +0300

    add main

---
+++ src/Main/Main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Main.module.css';
+
+function Main() {
+  return (
+    <div className={styles.Main}>
+      2
+    </div>
+  );
+}
+
+export default Main;
