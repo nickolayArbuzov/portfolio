86e20f98f376ba3ea178fc64bfc8cb6bae462034
nickolayArbuzov <nickarbuzov@yandex.by>
2019-10-20 23:57:04 +0300

    add 3&4 block

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Header from './Header/Header';
 import Main from './Main/Main';
+import Skills from './Skills/Skills';
+import Works from './Works/Works';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Main/>
+      <Skills/>
+      <Works/>
     </div>
   );
 }
---
+++ src/Works/Works.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Works.module.css';
+
+function Works() {
+  return (
+    <div className={styles.Works}>
+      4
+    </div>
+  );
+}
+
+export default Works;
