40b35f048dd38f21ea22789c1416c40658722f25
nickolayArbuzov <nickarbuzov@yandex.by>
2019-10-22 00:21:08 +0300

    add navBar for Header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -4,7 +4,12 @@ import styles from './Header.module.css';
 function Header() {
   return (
     <div className={styles.Header}>
-      1
+      <div className='nav'>
+        <a href='' className='link'>Главная</a>
+        <a href='' className='link'>Скиллы</a>
+        <a href='' className='link'>Проекты</a>
+        <a href='' className='link'>Контакты</a>
+      </div>
     </div>
   );
 }
