116a80987ac3bf894bc2f26425a158abb17bc18a
nickolayArbuzov <nickarbuzov@yandex.by>
2019-10-22 01:16:08 +0300

    add photo-area

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -5,7 +5,14 @@ function Main() {
   return (
     <div className={styles.main}>
       <div className={styles.container}>
-
+        <div className={styles.greet}>
+          <span>Привет!</span>
+          <span>Меня зовут Николай Арбузов</span>
+          <span>Я fron-end разработчик</span>
+        </div>
+        <div className={styles.photo}>
+          <img src='' alt=''/>
+        </div> 
       </div>
     </div>
   );
