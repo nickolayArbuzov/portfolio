4103082250ca63e8a5b2d55d17004cb4ebf17c7a
nickolayArbuzov <nickarbuzov@yandex.by>
2021-02-05 13:58:21 +0300

    lesson1

--- Desktop/it-incubator/src/App.tsx
+++ Desktop/it-incubator/src/App.tsx
@@ -1,26 +1,26 @@
 import React from 'react';
 import './App.css';
+import Todolist, { TaskType } from './Todolist';
 
 function App() {
+
+    let task1 = [
+        { id: 1, title: 'css', isDone: true },
+        { id: 2, title: 'html', isDone: true },
+        { id: 3, title: 'react', isDone: true },
+        { id: 4, title: 'redux', isDone: false }
+    ];
+    let task2: Array<TaskType> = [
+        { id: 1, title: 'Pred', isDone: true },
+        { id: 2, title: 'Term', isDone: true },
+        { id: 3, title: 'Com', isDone: true },
+        { id: 4, title: 'Robo', isDone: false }
+    ]
+
     return (
         <div className="App">
-            <div>
-                <h3>What to learn</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
+            <Todolist title="What to learn" tasks={task1}/>
+            <Todolist title="Movies" tasks={task2}/>
         </div>
     );
 }
