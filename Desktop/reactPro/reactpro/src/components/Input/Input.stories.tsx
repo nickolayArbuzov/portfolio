81de5a0b9ebc201e5a2b235e3272a5b59d77420b
nickolayArbuzov <nickarbuzov@yandex.by>
2021-02-19 01:26:01 +0300

    lesson13

--- Desktop/reactPro/reactpro/src/components/Input/Input.stories.tsx
+++ Desktop/reactPro/reactpro/src/components/Input/Input.stories.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useRef, ChangeEvent } from 'react';
+import { action } from '@storybook/addon-actions';
 import { Story, Meta } from '@storybook/react/types-6-0';
 
 export default {
@@ -29,6 +30,35 @@ export const GetValueUncontrolledInputByClickButton = () => {
   return(
     <><input ref={inputRef}/> <button onClick={getValue}>getValue</button> {value} </>
   )
+}
+
+export const ControlledInput = () => {
+  const [parentValue, setParentValue] = useState('');
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.value);
+  }
+  return <input value={parentValue} onChange={onChange}/>
+}
+
+export const ControlledCheckbox = () => {
+  const [parentValue, setParentValue] = useState(false);
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setParentValue(e.currentTarget.checked);
+  }
+  return <input type='checkbox' checked={parentValue} onChange={onChange}/>
+}
+
+export const ControlledSelect = () => {
+  const [parentValue, setParentValue] = useState<string | undefined>();
+  const onChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    setParentValue(e.currentTarget.value);
+  }
+  return (<select value={parentValue} onChange={onChange}>
+            <option>0</option>
+            <option value={1}>11</option>
+            <option value={2}>22</option>
+            <option value={3}>33</option>
+          </select>)
 }
 
 export const Input = () => <input value="it"/>
